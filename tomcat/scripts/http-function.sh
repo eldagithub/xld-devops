619fc57a36588251a0c691d265f5866f771f9c3d
elmos@MBP-de-elmos <elmos@MBP-de-elmos>
2021-11-28 14:50:32 +0100

    scripts at Dim 28 nov 2021 14:50:32 CET

--- tomcat/scripts/http-function.sh
+++ tomcat/scripts/http-function.sh
@@ -5,12 +5,14 @@ ACTION=$2
 APP=$SCRIPTNAME
 
 case "$ACTION" in
-  (start|start|reload) ;;
-  (*) echo "Please provide allowed input : stop|start|reload" && exit 11 ;;
+  (start|stop|reload|check|list) ;;
+  (*) echo "ERROR:   Please provide allowed input : stop|start|reload|check" && exit 11 ;;
 esac
 
+VERB=$ACTION
+[[ $VERB == "check" ]] && VERB=list
 
-URL="$URLTOMCAT/$ACTION?path=/$APP"
+URL="$URLTOMCAT/$VERB?path=/$APP"
 echo URL : $URL
 
 
@@ -19,9 +21,8 @@ set +x
     local url=$1
     local body=$2
 
-    [[ -z "$url" ]] || [[ -z "$body" ]] && echo "Input are not fully provided : httpcheckCodeBody url body" && return 12
+    [[ -z "$url" ]] || [[ -z "$body" ]] && echo "ERROR:  Input are not fully provided : httpcheckCodeBody url body" && return 12
 
-    #local http_response=$(curl $TOMCAT_CONNEXION -w "HTTPSTATUS:%{http_code}" --silent "$URL")
     local http_response=$(curl $CURL_OPTS --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
 
     local http_status=$(echo $http_response | tr -d '\n' | sed -E 's/.*HTTPSTATUS:([0-9]{3})$/\1/')
@@ -30,10 +31,18 @@ set +x
     echo http_status : $http_status
     echo http_body : $http_body
 
-    [[ $http_status != 200 ]] && echo http status not 200 && return 13
-    [[ $http_body != ${body} ]] && echo http body does not contains $body && return 14
-
+    [[ $http_status != 200 ]] && echo ERROR:   http status not 200 && return 13
+    [[ $http_body != ${body} ]] && echo ERROR:   http body does not contains $body && return 14
+    
+    echo "###################"
+    echo "SUCCESSFUL CALL"
+    echo "###################"
     return 0
 }
 
-
+if [ $ACTION == "check" ]
+then
+ httpcheckCodeBody $URL *$APP:running*
+else
+ httpcheckCodeBody $URL OK*
+fi
