6ab2804a375703fc0723e2edb798c2b8cb4140c9
elmos@MBP-de-elmos <elmos@MBP-de-elmos>
2021-11-28 14:55:16 +0100

    scripts at Dim 28 nov 2021 14:55:16 CET

---
+++ tomcat/scripts/pickup-manager-toto.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#NOT NEED TO CUSTOM
+#Generic script based only script name :
+#action_application-name.sh
+#action : stop|start|reload
+#application-name : application name without "_"
+
+
+cd "$(dirname "$0")"
+APP=$(basename -s .sh -- "$0")
+. ./http-function.sh $APP $1
+
+
